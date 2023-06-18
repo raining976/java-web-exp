40d97abc3c39df369a77c5fa706b2a8be42b80fa
raining <running.zhang@foxmail.com>
2023-06-18 23:08:48 +0800

    feat：添加注册接口

--- src/main/java/raining/java_web_exp/controller/UserController.java
+++ src/main/java/raining/java_web_exp/controller/UserController.java
@@ -2,7 +2,6 @@ package raining.java_web_exp.controller;
 
 import jakarta.servlet.http.Cookie;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,48 +15,48 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import raining.java_web_exp.model.*;
 import raining.java_web_exp.db.Conn;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	private Conn conn = new Conn();
+	
+	// 登陆接口
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody  User user, HttpSession session, HttpServletResponse res) {
+	public ResponseEntity<String> login(@RequestBody User user, HttpSession session, HttpServletResponse res) {
 		String formPass = user.getPassword();
 		User userEntity = conn.getUserByUsername(user.getUsername());
 		if (userEntity != null && userEntity.getPassword().equals(formPass)) {
-			session.setAttribute("USER_INFO", user);
-			Cookie cookie = new Cookie("USER_ID",user.getUsername());
-			int maxAge = 10*60;
+			session.setAttribute("USER_INFO", userEntity);
+			Cookie cookie = new Cookie("USER_ID", user.getUsername());
+			int maxAge = 10 * 60;
 			cookie.setMaxAge(maxAge); // 有效期
 			cookie.setPath("/"); // 设置Cookie的作用范围为整个应用程序
-			Cookie expireCookie = new Cookie("MAX_AGE","600"); // 过期时间的cookie 
+			Cookie expireCookie = new Cookie("MAX_AGE", "600"); // 过期时间的cookie
 			expireCookie.setMaxAge(maxAge); // 有效期
 			expireCookie.setPath("/"); // 设置Cookie的作用范围为整个应用程序
 			res.addCookie(cookie);
 			res.addCookie(expireCookie);
-			return ResponseEntity.ok("success");
+			return ResponseEntity.ok("登陆成功啦！");
 		}
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("fail");
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("密码不对哦");
 	}
-	
+
+	// 注册接口
+	@PostMapping("/register")
+	public ResponseEntity<String> login(@RequestBody User user, HttpSession session) {
+		boolean flag  = this.conn.registerUser(user);
+		if(flag) {
+			return ResponseEntity.ok("注册成功啦！");
+		}
+		return ResponseEntity.status(HttpStatus.CONFLICT).body("用户名已被注册了哦");
+	}
+
+	// 获取用户信息接口
 	@GetMapping("/info")
 	public User userinfo(@RequestParam("name") String username, HttpSession session) {
 		return conn.getUserByUsername(username);
 	}
+
 	
-	
-//	@GetMapping("/userList")
-//	public List<UserEntity> sayHello() {
-//
-//		UserEntity userEntity = new UserEntity();
-//
-//		userEntity.setUsername("Xiaodong");
-//		userEntity.setPassword("123456");
-//		userEntity.setCreateDate("today");
-//
-//		List<UserEntity> userEntities = new ArrayList<UserEntity>();
-//		userEntities.add(userEntity);
-//
-//		return userEntities;
-//	}
 }
